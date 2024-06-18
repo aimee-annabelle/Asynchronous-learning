7b760d3b8201a3913f8f03b35fb4217ac1fba7c7
aimee-annabelle <inezannabelle@gmail.com>
2024-06-18 11:16:33 +0200

    async await

--- async/fetchapi.js
+++ async/fetchapi.js
@@ -1,4 +1,10 @@
-fetch("https://jsonplaceholder.typicode.com/todos/")
-  .then((response) => response.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
+// fetch("https://jsonplaceholder.typicode.com/todos/")
+//   .then((response) => response.json())
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err));
+
+const getTodos = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await response.json();
+  return data;
+};
