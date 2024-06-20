31a9478371fd4a5f149ed7a6393ca717c419b90c
aimee-annabelle <inezannabelle@gmail.com>
2024-06-20 15:09:38 +0200

    bg color change with setInterval

--- practice/multipleAPIs.js
+++ practice/multipleAPIs.js
@@ -1,20 +1,31 @@
 const fetchMultipleAPIs = async(urls) => {
-    let fetchApi = await
-    urls.map(el => {
-        fetch(el)
-    });
+  let fetchSingle = async(url)=>{
+    let response = await fetch(url);
+    return await response.json();
+  }
+  let requests = urls.map(el => fetchSingle(el));
+  return requests;
+
+  // let requests = urls.map(el => fetch(el));
+  // let result = [];
+//   Promise.all(requests).then(responses =>{
+//     responses.forEach(response => response.json())
+//     result = responses;
+//     return responses
+//   })
+// return result;
 }
 
 const apiUrls = [
-    'https://jsonplaceholder.typicode.com/posts/4',
-    'https://jsonplaceholder.typicode.com/posts/5',
-    'https://jsonplaceholder.typicode.com/posts/6'
-  ];
-  
-  fetchMultipleAPIs(apiUrls)
-    .then(results => {
-      console.log('Combined Results:', results);
-    })
-    .catch(error => {
-      console.log('Error:', error.message);
-    });
+  'https://jsonplaceholder.typicode.com/todos/1',
+  'https://jsonplaceholder.typicode.com/todos/5',
+  'https://jsonplaceholder.typicode.com/todos/6'
+];
+
+fetchMultipleAPIs(apiUrls)
+  .then(results => {
+    console.log('Combined Results:', results);
+  })
+  .catch(error => {
+    console.log('Error:', error.message);
+  });
