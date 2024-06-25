aab8110b13f4eb470ae0e761f3989d888a033196
aimee-annabelle <inezannabelle@gmail.com>
2024-06-25 17:22:53 +0200

    alarm

---
+++ mental-modal/alarm.js
@@ -0,0 +1,15 @@
+const createAlarm = (name, time) => {
+  return new Promise((resolve, reject) => {
+    if (time < 2) reject("Delay is not sufficient");
+    setTimeout(() => {
+      resolve(`Wake up ${name}`);
+    }, time * 1000);
+  });
+};
+
+createAlarm('John', 4).then((message) => {
+    console.log(message) // output "Wake up John" after 4 seconds
+}).catch((error) => {
+    console.error(error)
+})
+
